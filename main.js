05a112580d4ce88d296c82ded4ac61c9c8a4c3e8
FlyingColors02 <shraduborse97@gmail.com>
2020-02-03 15:47:27 +0530

    #shraddha borse: BODMAS rule in JS

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ console.log(result);
 result= (numValue1 + numValue2) * numValue3;//25
 console.log(result);
 
+let strValue1="4";
+result= numValue1 + numValue2 + strValue1 + numValue3;
+console.log(result);//545
+/*first it will add numValue1 and numValue2 
+but as it finds strValue1 it will convert everything after that into string*/ 
+
+//another example
+
+result= numValue1 + strValue1 + numValue2 + numValue3;
+console.log(result);//2435
+//didn't add numValue2 and numValue3
