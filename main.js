d1cd1f8de9790ce9e7856c572cd00f586ad6b368
FlyingColors02 <shraduborse97@gmail.com>
2020-02-03 15:03:00 +0530

    #shraddha borse: BODMAS rule in js

---
+++ main.js
@@ -0,0 +1,12 @@
+//BODMAS Rule in JS
+
+let numValue1=2;
+let numValue2=3;
+let numValue3=5;
+
+let result= numValue1 + numValue2 * numValue3;//17
+console.log(result);
+
+result= (numValue1 + numValue2) * numValue3;//25
+console.log(result);
+
